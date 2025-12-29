ebfef3a74cfae66857edcadc6ae0c9d84ebeb3f7
Rafael Caetano <rafaelvcaetano@hotmail.com>
2025-12-29 16:27:17 +0000

    Allow AndroidFileHandler to receive the raw FileMode

--- src/android/AndroidFileHandler.h
+++ src/android/AndroidFileHandler.h
@@ -2,12 +2,13 @@
 #define MELONDS_ANDROID_ANDROIDFILEHANDLER_H
 
 #include <stdio.h>
+#include <Platform.h>
 
 namespace MelonDSAndroid
 {
     class AndroidFileHandler {
     public:
-        virtual FILE* open(const char* path, const char* mode) = 0;
+        virtual FILE* open(const char* path, melonDS::Platform::FileMode mode) = 0;
         virtual ~AndroidFileHandler() {};
     };
 }
